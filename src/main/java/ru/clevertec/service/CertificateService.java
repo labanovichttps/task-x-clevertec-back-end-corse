88b198211b62a0034e74dae7efabcb61e52a9122
loverloser <timofeilobanovich@gmail.com>
2022-04-12 15:17:47 +0300

    edit gitignore

--- src/main/java/ru/clevertec/service/CertificateService.java
+++ src/main/java/ru/clevertec/service/CertificateService.java
@@ -1,16 +1,12 @@
 package ru.clevertec.service;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-import ru.clevertec.entity.Certificate;
 import ru.clevertec.entity.dto.CertificateDto;
 import ru.clevertec.mapper.CertificateMapper;
 import ru.clevertec.repository.CertificateRepository;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static java.util.stream.Collectors.toList;
 
